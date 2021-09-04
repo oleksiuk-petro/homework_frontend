c87f656ff53f910b228c64c16a0ef9f91330cfe3
oleksiuk-petro <oleksiuk.petro1993@gmail.com>
2021-09-04 23:47:07 +0300

    I writed homework_07 (JavaScript).

--- hw-js/homework_07/script_01.js
+++ hw-js/homework_07/script_01.js
@@ -18,3 +18,31 @@
             <li></li>
         </ul>
 */
+
+// for random text (size is 20 symbols)
+function str_rand() {
+    var result = '';
+    var words = '0123456789 qwertyuiop asdfghjklz xcvbnmQWER TYUIOPASDF GHJKLZXCVBNM';
+    var max_position = words.length - 1;
+    for (i = 0; i < 20; ++i) {
+        position = Math.floor(Math.random() * max_position);
+        result = result + words.substring(position, position + 1);
+    }
+    return result;
+}
+
+// for click button
+pressButton.onclick = function () {
+    let elementsArray = document.getElementsByTagName('li');
+    for (let i = 0; i < elementsArray.length; i++) {
+        elementsArray[i].innerHTML = str_rand();
+    }
+}
+
+// for showing number 'li' elements
+let numbers = 0;
+let elementsArray = document.getElementsByTagName('li');
+for (let i = 0; i < elementsArray.length; i++) {
+    numbers++;
+}
+console.log('Number \'li\' elements is: ' + numbers)
