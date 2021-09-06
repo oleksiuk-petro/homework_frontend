c95dea2a19e7e4d7c9ad63fa2d02283a2e787dc0
oleksiuk-petro <oleksiuk.petro1993@gmail.com>
2021-09-06 14:28:23 +0300

    I writed homework_08 (JavaScript).

--- hw-js/homework_08/script.js
+++ hw-js/homework_08/script.js
@@ -7,3 +7,27 @@
         ● ondoubleclick
         ● onfocus
 */
+
+function functionForOnscroll() {
+    document.getElementById('forOnscroll').style.color = 'red';
+}
+
+function functionForOnsubmit() {
+    alert('Server got your text!');
+}
+
+function functionForOnreset() {
+    alert('Input field will be reset!');
+}
+
+function functionForOncontextmenu() {
+    alert('Right-clicked inside the div-element!');
+}
+
+function functionForOndblclick() {
+    document.getElementById('someMessage').innerHTML = "You recently have double-click on div-element!";
+}
+
+function fuctionForOnfocus(x) {
+    x.style.background = 'orange';
+}
